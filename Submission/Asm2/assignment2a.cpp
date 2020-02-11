12f21c8a5e7e3212b8fbc569f34bae290b2c4486
Milo <ton.minhuyen@gmail.com>
2020-02-11 14:44:23 +0700

    Clear all in ASM2

--- Submission/Asm2/assignment2a.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main() {
-    int a,b;
-    std::cout<<"Enter 2 integers \n";
-    std::cin>>a;
-    std::cin>>b;
-    std::cout<<a+b;
-}
--- Submission/Asm2/assignment2d.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cmath>
-int main() {
-    int a,b;
-    std::cout<<"Enter 2 integers \n";
-    std::cin>>a;
-    std::cin>>b;
-    std::cout<<pow(a,b);
-}
